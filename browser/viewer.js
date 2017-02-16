2dee33f5761c5d9a93953c3af5cdfa472e403934
RaulJimenezOrtega <hhkaos@gmail.com>
2017-02-16 17:31:40 +0100

    Fixing browser samples

--- browser/viewer.js
+++ browser/viewer.js
@@ -1,50 +1,41 @@
+'use strict';
+
+var map;
+
 require([
-  "dojo/query",
-  "terraformer/terraformer",
-  "terraformer/wkt",
-  "terraformer/arcgis",
-  "esri/map"
-], function (query,Terraformer, TerraformerWKT, TerraformerArcGIS) {
-  var map = new esri.Map("map", {
-    basemap: "topo",
+  'esri/map',
+  'esri/Color',
+  'esri/graphic',
+  'esri/symbols/SimpleLineSymbol',
+  'esri/symbols/SimpleFillSymbol',
+  'esri/geometry/Geometry',
+  'dojo/query'
+], function (Map, Color, Graphic, SimpleLineSymbol, SimpleFillSymbol, Geometry, query) {
+
+  map = new Map('map', {
+    basemap: 'topo',
     center: [-122, 45],
     zoom: 6
   });
 
-  var hullOutline = new esri.symbol.SimpleLineSymbol("dash",new dojo.Color([47,140,212]), 2);
-  var boundsOutline = new esri.symbol.SimpleLineSymbol("dash",new dojo.Color([212,125,47]), 2);
-  var hullSymbol = new esri.symbol.SimpleFillSymbol("solid", hullOutline, new dojo.Color([195,229,255,0.5]));
-  var boundsSymbol = new esri.symbol.SimpleFillSymbol("solid", boundsOutline, new dojo.Color([255,215,180,0.5]));
+  var hullOutline = new SimpleLineSymbol('dash',new Color([47,140,212]), 2);
+  var boundsOutline = new SimpleLineSymbol('dash',new Color([212,125,47]), 2);
+  var hullSymbol = new SimpleFillSymbol('solid', hullOutline, new Color([195,229,255,0.5]));
+  var boundsSymbol = new SimpleFillSymbol('solid', boundsOutline, new Color([255,215,180,0.5]));
 
   var currentPrimitive;
   var shape;
-  function showArcGIS(arcgis){
-    currentPrimitive = TerraformerArcGIS.parse(arcgis);
-    addGraphic(arcgis);
-  }
 
-  function showWKT(wkt){
-    currentPrimitive = TerraformerWKT.parse(wkt);
-    var arcgis = TerraformerArcGIS.convert(currentPrimitive);
-    addGraphic(arcgis);
-  }
 
-  function showGeoJSON(geojson){
-    // convert the geojson object to a arcgis json representation
-    currentPrimitive = new Terraformer.Primitive(geojson);
-    var arcgis = TerraformerArcGIS.convert(currentPrimitive);
-    addGraphic(arcgis);
-  }
-
-  function addGraphic(arcgis){
+  var addGraphic = function (arcgis){
     // if arcgis.geometry is set we have a graphic json
     // else we can create our own json and set the symbol on it.
     if(arcgis.geometry){
-      shape = new esri.Graphic(arcgis).setSymbol(new esri.symbol.SimpleFillSymbol());
+      shape = new Graphic(arcgis).setSymbol(new SimpleFillSymbol());
     } else {
-      shape = new esri.Graphic({
+      shape = new Graphic({
         geometry: arcgis
-      }).setSymbol(new esri.symbol.SimpleFillSymbol());
+      }).setSymbol(new SimpleFillSymbol());
     }
 
     // add the graphic to the map
@@ -52,25 +43,49 @@ require([
 
     // center the map on the graphic
     map.setExtent(shape.geometry.getExtent());
-  }
+  };
+
+  var showArcGIS = function(arcgis){
+    currentPrimitive = Terraformer.ArcGIS.parse(arcgis);
+    addGraphic(arcgis);
+  };
+
+  var showWKT = function(wkt){
+    currentPrimitive = Terraformer.WKT.parse(wkt);
+    var arcgis = Terraformer.ArcGIS.convert(currentPrimitive);
+    addGraphic(arcgis);
+  };
+
+  var showGeoJSON = function(geojson){
+    // convert the geojson object to a arcgis json representation
+    currentPrimitive = new Terraformer.Primitive(geojson);
+    var arcgis = Terraformer.ArcGIS.convert(currentPrimitive);
+    addGraphic(arcgis);
+  };
 
   function showOnMap(){
     map.graphics.clear();
 
     // parse the input as json
-    var input = query(".tab-pane.active textarea").attr("value")[0];
-    var type = query(".tab-pane.active textarea").attr("data-type")[0];
+    var input = query('.tab-pane.active textarea').attr('value')[0];
+    var type = query('.tab-pane.active textarea').attr('data-type')[0];
 
 
     switch(type){
-    case "wkt":
-      showWKT(input);
+    case 'wkt':
+      try{
+        showWKT(input);
+      }catch(e){ alert("This is not a WKT valid format");}
       break;
-    case "geojson":
-      showGeoJSON(JSON.parse(input));
+    case 'geojson':
+      try{
+        showGeoJSON(JSON.parse(input));
+      }catch(e){ alert("This is not a GeoJSON valid format");}
       break;
-    case "arcgis":
-      showArcGIS(JSON.parse(input));
+    case 'arcgis':
+      try{
+        showArcGIS(JSON.parse(input));
+      }catch(e){ alert("This is not an ArcGIS valid format");}
       break;
     }
   }
@@ -81,13 +96,13 @@ require([
     var convex = new Terraformer.Polygon([ currentPrimitive.convexHull() ]);
 
     // convert the geojson object to a arcgis json representation
-    var arcgis = TerraformerArcGIS.convert(convex);
+    var arcgis = Terraformer.ArcGIS.convert(convex.coordinates[0]);
 
     // create a new geometry object from json
-    var geometry = esri.geometry.fromJson(arcgis);
+    var geometry = Geometry.fromJson(arcgis);
 
     // make a new graphic to put on the map
-    var gfx = new esri.Graphic(geometry, hullSymbol);
+    var gfx = new Graphic(geometry, hullSymbol);
 
     // add the graphic to the map
     map.graphics.add(gfx);
@@ -100,19 +115,19 @@ require([
 
   function showBBoxOnMap(){
 
-    var box = currentPrimitive.bbox;
+    var box = currentPrimitive.bbox();
 
     // create a bbox hull
     var bbox = new Terraformer.Polygon( [ [ [ box[0], box[1] ], [ box[0], box[3] ], [ box[2], box[3] ], [ box[2], box[1] ], [ box[0], box[1] ]  ] ] );
 
     // convert the geojson object to a arcgis json representation
-    var arcgis = TerraformerArcGIS.convert(bbox);
+    var arcgis = Terraformer.ArcGIS.convert(bbox);
 
     // create a new geometry object from json
-    var geometry = esri.geometry.fromJson(arcgis);
+    var geometry = Geometry.fromJson(arcgis);
 
     // make a new graphic to put on the map
-    var gfx = new esri.Graphic(geometry, boundsSymbol);
+    var gfx = new Graphic(geometry, boundsSymbol);
 
     // add the graphic to the map
     map.graphics.add(gfx);
@@ -123,10 +138,10 @@ require([
     map.setExtent(gfx.geometry.getExtent());
   }
 
-  query("#submit").on("click", showOnMap);
-  query("#bounding").on("click", showBBoxOnMap);
-  query("#convex").on("click", showConvexOnMap);
-  query("#clear").on("click", function(){
+  query('#submit').on('click', showOnMap);
+  query('#bounding').on('click', showBBoxOnMap);
+  query('#convex').on('click', showConvexOnMap);
+  query('#clear').on('click', function(){
     map.graphics.clear();
   });
 });
